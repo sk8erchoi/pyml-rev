b569028c86d8462a415e36050904f0e6725c73f2
Yong Choi <sk8er.choi@gmail.com>
2021-07-02 01:33:06 +0900

    Update ch7

--- ch7/gyudon-makedata2.py
+++ ch7/gyudon-makedata2.py
@@ -1,7 +1,9 @@
 from PIL import Image
-import os, glob
+import glob
 import numpy as np
 import random, math
+
+
 # 분류 대상 카테고리
 root_dir = "./image/"
 categories = ["normal", "beni", "negi", "cheese"]
@@ -10,6 +12,8 @@ image_size = 50
 # 이미지 데이터 읽어 들이기 --- (※1)
 X = [] # 이미지 데이터
 Y = [] # 레이블 데이터
+
+
 def add_sample(cat, fname, is_train):
     img = Image.open(fname)
     img = img.convert("RGB") # 색상 모드 변경하기
@@ -31,17 +35,21 @@ def add_sample(cat, fname, is_train):
         data = np.asarray(img2)
         X.append(data)
         Y.append(cat)
+
+
 def make_sample(files, is_train):
     global X, Y
     X = []; Y = []
     for cat, fname in files:
         add_sample(cat, fname, is_train)
     return np.array(X), np.array(Y)
+
+
 # 각 폴더에 들어있는 파일 수집하기 --- (※2)
 allfiles = []
 for idx, cat in enumerate(categories):
     image_dir = root_dir + "/" + cat
-    files = glob.glob(image_dir + "/*.PG")
+    files = glob.glob(image_dir + "/*.jpg")
     for f in files:
         allfiles.append((idx, f))
 # 섞은 뒤에 학습 전용 데이터와 테스트 전용 데이터 구분하기 --- (※3)
--- ch7/gyudon_keras.py
+++ ch7/gyudon_keras.py
@@ -1,37 +1,44 @@
-from keras.models import Sequential
-from keras.layers import Convolution2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.utils import np_utils
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Convolution2D, MaxPooling2D
+from tensorflow.keras.layers import Activation, Dropout, Flatten, Dense
+from tensorflow.keras import utils
 import numpy as np
+
+
 # 분류 대상 카테고리
 root_dir = "./image/"
 categories = ["normal", "beni", "negi", "cheese"]
 nb_classes = len(categories)
 image_size = 50
+
+
 # 데이터 다운로드하기 --- (※1)
 def main():
-    X_train, X_test, y_train, y_test = np.load("./image/gyudon.npy")
+    #X_train, X_test, y_train, y_test = np.load("./image/gyudon.npy")
+    X_train, X_test, y_train, y_test = np.load("./image/gyudon2.npy")
     # 데이터 정규화하기
     X_train = X_train.astype("float") / 256
     X_test  = X_test.astype("float")  / 256
-    y_train = np_utils.to_categorical(y_train, nb_classes)
-    y_test  = np_utils.to_categorical(y_test, nb_classes)
+    y_train = utils.to_categorical(y_train, nb_classes)
+    y_test  = utils.to_categorical(y_test, nb_classes)
     # 모델을 훈련하고 평가하기
     model = model_train(X_train, y_train)
     model_eval(model, X_test, y_test)
+
+
 # 모델 구축하기 --- (※2)
 def build_model(in_shape):
     model = Sequential()
     model.add(Convolution2D(32, 3, 3, 
-        border_mode='same',
+        padding='same',
         input_shape=in_shape))
     model.add(Activation('relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(0.25))
-    model.add(Convolution2D(64, 3, 3, border_mode='same'))
+    model.add(Convolution2D(64, 3, 3, padding='same'))
     model.add(Activation('relu'))
     model.add(Convolution2D(64, 3, 3))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2), padding='same'))
     model.add(Dropout(0.25))
     model.add(Flatten()) 
     model.add(Dense(512))
@@ -43,18 +50,24 @@ def build_model(in_shape):
         optimizer='rmsprop',
         metrics=['accuracy'])
     return model
+
+
 # 모델 훈련하기 --- (※3)
 def model_train(X, y):
     model = build_model(X.shape[1:])
-    model.fit(X, y, batch_size=32, nb_epoch=30)
+    model.fit(X, y, batch_size=32, epochs=30)
     # 모델 저장하기 --- (※4)
     hdf5_file = "./image/gyudon-model.hdf5"
     model.save_weights(hdf5_file)
     return model
+
+
 # 모델 평가하기 --- (※5)
 def model_eval(model, X, y):
     score = model.evaluate(X, y)
     print('loss=', score[0])
     print('accuracy=', score[1])
+
+
 if __name__ == "__main__":
     main()
