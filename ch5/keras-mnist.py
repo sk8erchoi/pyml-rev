36756612e5e9f2d6b298ef880d85075e5337c4db
Yong Choi <sk8er.choi@gmail.com>
2021-07-01 13:34:56 +0900

    Update keras-mnist.py

--- ch5/keras-mnist.py
+++ ch5/keras-mnist.py
@@ -1,8 +1,8 @@
-from keras.datasets import mnist
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation
-from keras.optimizers import Adam 
-from keras.utils import np_utils
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Activation
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras import utils
 # MNIST 데이터 읽어 들이기 --- (※1)
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 # 데이터를 float32 자료형으로 변환하고 정규화하기 --- (※2)
@@ -11,8 +11,8 @@ X_test  = X_test.reshape(10000, 784).astype('float')
 X_train /= 255
 X_test  /= 255
 # 레이블 데이터를 0-9까지의 카테고리를 나타내는 배열로 변환하기 --- (※2a)
-y_train = np_utils.to_categorical(y_train, 10)
-y_test  = np_utils.to_categorical(y_test, 10)
+y_train = utils.to_categorical(y_train, 10)
+y_test  = utils.to_categorical(y_test, 10)
 # 모델 구조 정의하기 --- (※3)
 model = Sequential()
 model.add(Dense(512, input_shape=(784,)))
