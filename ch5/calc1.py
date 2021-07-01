0772e545c927838192786fd2b14624f64919b1d3
Yong Choi <sk8er.choi@gmail.com>
2021-07-01 13:37:41 +0900

    Merge ch5 with ch5_tf2

--- ch5/calc1.py
+++ ch5/calc1.py
@@ -1,14 +1,17 @@
-# TensorFlow 읽어 들이기 --- (※1)
-import tensorflow.compat.v1 as tf
-
-tf.compat.v1.disable_eager_execution()
+# TensorFlow 임포트 --- (※1)
+import tensorflow as tf
 
 # 상수 정의 --- (※2)
 a = tf.constant(1234)
 b = tf.constant(5000)
+
+
 # 계산 정의 --- (※3)
-add_op = a + b
+@tf.function
+def add_op(a, b):
+    return a + b
+
+
 # 세션 시작하기 --- (※4)
-sess = tf.Session()
-res = sess.run(add_op) # 식 평가하기
+res = add_op(a, b).numpy()        # 식 평가하기
 print(res)
