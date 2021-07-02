36ad36823d1839ce243a285cbf293541de7aa0f3
Yong Choi <sk8er.choi@gmail.com>
2021-07-02 09:33:36 +0900

    Update facedetect-mosaic.py
    - 정규표현식 오류 수정
    - 캐스케이드 파일 경로 설정

--- ch7/facedetect-mosaic.py
+++ ch7/facedetect-mosaic.py
@@ -5,10 +5,12 @@ if len(sys.argv) <= 1:
     quit()
 image_file = sys.argv[1] 
 # 출력 파일 이름
-output_file = re.sub(r'\,jpg|jpeg|PNG$', '-mosaic.jpg', image_file)
+output_file = re.sub(r'\.jpg|jpeg|PNG$', '-mosaic.jpg', image_file)
+print(output_file)
 mosaic_rate = 30 
 # 캐스캐이드 파일 경로 지정하기
-cascade_file = "haarcascade_frontalface_alt.xml"
+cascade_file = cv2.data.haarcascades + "haarcascade_frontalface_alt.xml"
+
 # 이미지 읽어 들이기 --- (※2)
 image = cv2.imread(image_file)
 image_gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # 그레이스케일 변환
