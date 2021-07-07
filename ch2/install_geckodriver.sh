81f6bb4dea1e8adeddaf4cb25cd9b59d82d64502
Yong Choi <sk8er.choi@gmail.com>
2021-07-07 17:50:05 +0900

    Create install_geckodriver.sh

---
+++ ch2/install_geckodriver.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/src && cd ~/src
+wget https://github.com/mozilla/geckodriver/releases/download/v0.21.0/geckodriver-v0.21.0-linux64.tar.gz
+tar -zxvf geckodriver-v0.21.0-linux64.tar.gz
+sudo mv geckodriver /usr/local/bin/
