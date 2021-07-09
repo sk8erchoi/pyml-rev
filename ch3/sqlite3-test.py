2abe02d4ed5eb0bccb7cd38049a1aeddb4e66c33
Yong Choi <sk8er.choi@gmail.com>
2021-07-09 17:26:37 +0900

    Update sqlite3-test.py

--- ch3/sqlite3-test.py
+++ ch3/sqlite3-test.py
@@ -1,7 +1,10 @@
 import sqlite3
+
+
 # sqlite 데이터베이스 연결하기 --- (※1)
 dbpath = "test.sqlite"
 conn = sqlite3.connect(dbpath)
+
 # 테이블을 생성하고 데이터 넣기 --- (※2)
 cur = conn.cursor()
 cur.executescript("""
@@ -18,12 +21,16 @@ INSERT INTO items(name, price)VALUES('Apple', 800);
 INSERT INTO items(name, price)VALUES('Orange', 780);  
 INSERT INTO items(name, price)VALUES('Banana', 430);
 """)
+
 # 위의 조작을 데이터베이스에 반영하기 --- (※3)
 conn.commit()
+
 # 데이터 추출하기 --- (※4)
 cur = conn.cursor()
 cur.execute("SELECT item_id,name,price FROM items")
 item_list = cur.fetchall()
+
 # 출력하기
 for it in item_list:
     print(it)
+
